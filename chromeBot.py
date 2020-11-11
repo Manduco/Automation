6dcc1305c7f3886a85b8773233644621a2c95379
Armando Zincke <49289761+Manduco@users.noreply.github.com>
2020-11-11 17:29:16 -0500

    Update chromeBot.py

--- chromeBot.py
+++ chromeBot.py
@@ -1,5 +1,11 @@
 from selenium import webdriver
 
-browser = webdriver.Chrome('/usr/local/bin/chromedriver')
-browser.get("https://elmocheroausente.com/")
-#testing
+Purchased_Made = false;
+
+
+driver = webdriver.Chrome()
+driver.get("https://www.bestbuy.com/site/msi-optix-g27c5-27-led-monitor-black/6425570.p?skuId=6425570")
+
+
+
+## Now iterate through them and check for our desired match
