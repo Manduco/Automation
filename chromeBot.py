7af096b66e5f5f58ee1d0a82f1528d8eedf066c2
Armando Zincke <49289761+Manduco@users.noreply.github.com>
2020-11-12 10:03:42 -0500

    Update chromeBot.py

--- chromeBot.py
+++ chromeBot.py
@@ -1,11 +1,12 @@
 from selenium import webdriver
 
-Purchased_Made = false;
+Purchased_Made = False;
 
 
 driver = webdriver.Chrome()
 driver.get("https://www.bestbuy.com/site/msi-optix-g27c5-27-led-monitor-black/6425570.p?skuId=6425570")
 
-
+element = driver.find_element_by_css_selector("input.btn")
+element.click()
 
 ## Now iterate through them and check for our desired match
