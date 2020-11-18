9945c5272c2ff3a09242a731df269e34cc4291bc
Armando Zincke <49289761+Manduco@users.noreply.github.com>
2020-11-18 12:23:45 -0500

    morning push

--- randomcode.py
+++ randomcode.py
@@ -2,9 +2,135 @@
 # rows, cols = (3, 2)
 # arr = [[0]*cols]*rows
 #print(arr)
-x = int(input())
-y = int(input())
-x= x%y
-x= x%y
-y= y%x
-print(y)
+
+#quest1
+# def any():
+#     print(var+1,end='')
+# var =1
+# any()
+# print(var)
+
+# tup = (1,2,4,8)
+# tup = tup[-2:-1]
+# tup = tup[-1]
+# print(tup)
+
+# dct = {'one':'two','three':'one','two':'three',}
+# v = dct['one']
+# for k in range(len(dct)):
+#     v = dct[v]
+# print(v)
+
+#tuple[1] = tuple[1] + tuple[0]
+
+# def func1(a):
+#     return None
+# def func2(a):
+#     return func1(a) * func1(a)
+# print(func2(2))
+
+# def f(x):
+#     if x == 0:
+#         return 0
+#     return x + f(x-1)
+# print(f(3))
+
+# dct = { }
+# lst = {'a','b','c','d'}
+# for i in range(len(lst) - 1):
+#     dct[ lst [i] ] = ( lst[i], )
+# for i in sorted(dct.keys()):
+#     k = dct[i]
+#     print(k)
+# def func(a,b):
+#     return b**a
+# print(func(b=2, 2))
+
+
+# list = {'mary','had','a','dlittle','lamb'}
+# def list(lst):
+#     del lst[3]
+#     lst[3]='ram'
+
+# print(list(list))
+# # for i in range(len(lst) - 1):
+# #     dct[ lst [i] ] = ( lst[i], )
+
+# def function1(x=0):
+#     return x
+
+# print(function1(3,3))
+
+# def fun(x):
+#     x += 1
+#     return x
+
+# x =2
+# x = fun(x+1)
+# print(x)
+
+# def fun(inp=2, out=3):
+#     return inp * out
+# print(fun(out=4))
+
+# def fun(x):
+#     if x % 2 == 0:
+#         return 1
+#     else:
+#         return 2
+# print(fun(fun(2)))
+
+# lst = [i for i in range (-1,-2)]
+# print(lst)
+
+# x = 1
+# y = 2
+# x, y, z = x, x, y
+# z, y, z = x, y, z
+
+# print(x,y,z)
+
+# x = int(input())
+# y = int(input())
+
+# x = x% y
+# x = x% y
+# y = y% x
+# print(y)
+
+# a = 1
+# b = 0
+
+# a = a ^ b
+# b = a ^ b
+# a = a ^ b
+
+# print(a,b)
+
+# def fun(a,b,c=0):
+#     print(a,b,c)
+
+# fun(a=1)
+
+# dd = ("1":"0" , "0":"1")
+# for x in dd.vals():
+#     print(x,end="")
+
+# nums = [1,2,3]
+# vals = nums
+# del vals[:]
+# #  print(str(len(nums))+" "+ str(len(vals)))
+
+# def fun(x,y):
+#     if x == y:
+#         return x
+#     else:
+#         return fun(x, y=1)
+# print(fun(0,3))
+
+# lst = [1,2]
+# for v in range (2):
+#     lst.insert(-1,lst[v])
+
+# print(lst)
+print('hello','world')
