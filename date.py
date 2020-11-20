9e2a0389a6415575e7dadd37ca106dba28bb5923
Armando Zincke <49289761+Manduco@users.noreply.github.com>
2020-11-20 17:33:14 -0500

    Message(working on auto push project)

--- date.py
+++ date.py
@@ -1,9 +1,27 @@
-import datetime
-import os.path, time
+import datetime, os.path, time, json
 
+FileToTest = "file.txt"
 now = datetime.datetime.now()
+file_lastdateM = " -date-"
 
-print ("Current date and time : " +now.strftime("%Y-%m-%d %H:%M:%S"))
+#file data on files I files i want tracked
+#print ("Current date and time : " + now.strftime("%Y-%m-%d %H:%M:%S"))
+#file_lastdateM = str(time.ctime(os.path.getmtime(FileToTest)))
+#print(FileToTest +" Created: %s" % time.ctime(os.path.getctime(FileToTest)))
+#print(file_lastdateM)
 
-print("Last modified: %s" % time.ctime(os.path.getmtime("file.txt")))
-print("Created: %s" % time.ctime(os.path.getctime("file.txt")))
+with open('date.json') as f:
+  data = json.load(f)
+
+# Output: {'name': 'Bob', 'languages': ['English', 'Fench']}
+# print(data)
+#datetime.timedelta(now, )
+# x = with subkey ie while file
+
+data["Tracking_files"]["files"][0]["Last_Date_Mod"] = file_lastdateM
+print(data["Tracking_files"]["files"][0]["file_name"])
+print(data["Tracking_files"]["files"][0]["Last_Date_Mod"])
+
+#write update json to file
+with open('data.txt', 'w') as outfile:
+    json.dump(data,outfile, indent=2)
