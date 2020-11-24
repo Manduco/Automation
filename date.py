e80df2445b8b10afa607732566b28ab0811b37f6
Armando Zincke <49289761+Manduco@users.noreply.github.com>
2020-11-24 17:24:12 -0500

    adding for tracking

--- date.py
+++ date.py
@@ -1,28 +1,33 @@
 import datetime, os.path, time, json
 
 FileToTest = "file.txt"
-now = datetime.datetime.now()
-file_lastdateM = " -date-"
+CurrentDate = str(datetime.datetime.now())
+LastDayRan= "-date-"
+file_lastdateM = "-date-"
 file_madeOn = "startdate"
+file_Datecreated = "-dateMade-"
 
 #file data on files I files i want tracked
-#print ("Current date and time : " + now.strftime("%Y-%m-%d %H:%M:%S"))
 file_lastdateM = str(time.ctime(os.path.getmtime(FileToTest)))
-print(FileToTest +" Created: %s" % time.ctime(os.path.getctime(FileToTest)))
+file_Datecreated = str(time.ctime(os.path.getctime(FileToTest)))
+#print(FileToTest +" Created: %s" % time.ctime(os.path.getctime(FileToTest)))
 #print(file_lastdateM)
 
 with open('date.json') as f:
   data = json.load(f)
 
-# Output: {'name': 'Bob', 'languages': ['English', 'Fench']}
-# print(data)
-#datetime.timedelta(now, )
-# x = with subkey ie while file
+print(data["Tracking_files"]["Date_last_ran"])
 
 data["Tracking_files"]["files"][0]["Last_Date_Mod"] = file_lastdateM
+data["Tracking_files"]["files"][0]["Date_created"] = file_Datecreated
+data["Tracking_files"]["Date_last_ran"] = CurrentDate
+
 print(data["Tracking_files"]["files"][0]["file_name"])
 print(data["Tracking_files"]["files"][0]["Last_Date_Mod"])
 
 #write update json to file
 with open('date.json', 'w') as outfile:
     json.dump(data,outfile, indent=2)
+ 
+def compare():
+  print("just_testing")
