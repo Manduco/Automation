0c9725d9c11b4e6a76ff98288b6e3238d4632587
Armando Zincke <49289761+Manduco@users.noreply.github.com>
2020-11-28 12:13:41 -0500

    push

--- date.py
+++ date.py
@@ -12,12 +12,13 @@ file_lastdateM = str(time.ctime(os.path.getmtime(FileToTest)))
 file_Datecreated = str(time.ctime(os.path.getctime(FileToTest)))
 #print(FileToTest +" Created: %s" % time.ctime(os.path.getctime(FileToTest)))
 #print(file_lastdateM)
-
+#test
 with open('date.json') as f:
   data = json.load(f)
 
-print(data["Tracking_files"]["Date_last_ran"])
+print("sricpt last ran on: "+data["Tracking_files"]["Date_last_ran"])
 
+data["Tracking_files"]["files"][0]["Last_Date_Mod2"] = data["Tracking_files"]["files"][0]["Last_Date_Mod"] 
 data["Tracking_files"]["files"][0]["Last_Date_Mod"] = file_lastdateM
 data["Tracking_files"]["files"][0]["Date_created"] = file_Datecreated
 data["Tracking_files"]["Date_last_ran"] = CurrentDate
