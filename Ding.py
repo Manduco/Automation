dc65a079aa938b7a9aee6dc67fa5838b0133caa0
Armando Zincke <49289761+Manduco@users.noreply.github.com>
2020-12-02 16:20:26 -0500

    push

---
+++ Ding.py
@@ -0,0 +1,43 @@
+import time, os
+from playsound import playsound
+
+One_Day_In_Seconds = 86400
+Twelve_hours = One_Day_In_Seconds/2
+Six_hours = Twelve_hours/2
+Test_num = 500
+One_Secound = 1
+KeepRunning = True
+Reset_Timer = 60
+
+def main():
+    Count_At = int(30)
+
+    # while (KeepRunning):
+
+    #     Format_time = convert(Count_At)
+    #     time.sleep(One_Secound)
+    #     Count_At -= 1
+
+    #     print('\033[H\033[J', end='')
+    #     print("Time till Code Push ",end=" ")
+    #     print(":",str(Format_time))
+
+    if(Count_At == 0):
+        Count_At = Reset_Timer
+        playsound("ding.mp3")
+
+# converts seconds
+def convert(seconds): 
+    seconds = seconds % (24 * 3600) 
+    hour = seconds // 3600
+    seconds %= 3600
+    minutes = seconds // 60
+    seconds %= 60
+      
+    return "%d:%02d:%02d" % (hour, minutes, seconds)
+
+# __name__ 
+if __name__=="__main__": 
+    main()
+else:
+    print("end") 
