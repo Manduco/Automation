55c48ea7dab29e951b593fde0f1ab541198594f1
manduco <armando.zincke@gmail.com>
2021-05-04 22:00:07 -0400

    for mel

--- test.ps1
+++ test.ps1
@@ -1,22 +1,37 @@
+#made my manduco
+
 [DateTime]$start_time = "2021-04-15 00:00:00"
 [DateTime]$end_time = "2021-05-03 23:59:59"
 $desfolder = "C:\Users\arman\Desktop\testdir"
-$TESTDir = "C:\Users\arman\Desktop\testdir"
+$Root = "C:\Users\arman\Desktop\testdir"
 
-Get-ChildItem -path $TESTDir -Recurse -File | Foreach 
-{
-    $FileInc = 1
-  
-    if ($.lastwritetime -ge $start_time -and $.lastwritetime -le $end_time) {
-        If ($FileInc -eq 1) { 
-            move-item $.fullname $desfolder 
-        }
-        Else { 
-            $MovePath = Join-Path -Path $desfolder "$($.BaseName)($FileInc)$($.Extension)" 
-            Rename-Item -Path "c:\logfiles\daily_file.txt" -NewName "monday_file.txt"
-            #change stuff here
-       
-        }
+$i = 0 
+
+Get-ChildItem -path $Root -Recurse -File | ForEach-Object -Process { if ($_.lastwritetime -ge $start_time -and $_.lastwritetime -le $end_time ) {
+
+
+        #rename file function here
+        #just move file function
+        $currentFile_Name = [string]$_
+        #write-host $currentFile_Name
+        Change_nameOfFile $currentFile_Name
+
+        $i = $i + 1
     }
+
 }
+
+
+Function Change_nameOfFile($currentFile_Name) {
+
+    #$New_name = -join($Old_name , " ", "world");
+    #Rename-Item -Path "c:\logfiles\daily_file.txt" -NewName "monday_file.txt"
+
+    write-host $currentFile_Name 
+
+}
+
+
+
+
 
